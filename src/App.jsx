bcc41ba3d94bcdd42a1efd46643d3f741f586765
Anji-fdo <131850714+Anji-fdo@users.noreply.github.com>
2024-01-06 11:17:18 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Signin from './pages/Signin';
+import Header from './components/Header';
 
 export default function App() {
-  return (<BrowserRouter>
+  return (
+  <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
