dbfbdffe869d5fe2ec72f92ee9bce9b4e085860d
Anji-fdo <131850714+Anji-fdo@users.noreply.github.com>
2024-01-08 10:50:35 +0530

    update the header and profile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
+import PrivateRoute from './components/privateRoute';
 
 export default function App() {
   return <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
       <Route path="/sign-in" element={<Signin />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      
     </Routes>
     </BrowserRouter>
   
